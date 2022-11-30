f45ae14ce342cba4a7c13cfe47245e633acce0f4
Amit Tailor <84769372+amittailor15@users.noreply.github.com>
2022-11-30 16:24:49 +0530

    Update RepeatedCharacter.java

--- RepeatedCharacter.java
+++ RepeatedCharacter.java
@@ -10,16 +10,20 @@ public class RepeatedCharacter
 	 public static void main(String[] args) {
 		 Scanner sc=new Scanner(System.in);
 		 String str1=sc.nextLine();
+		 int count;
 	  System.out.println("The given string is: " + str1);
-	  char[] carray = str1.toCharArray();
+	  char[] string = str1.toCharArray();
       System.out.print("Repeated Characters in string are: ");
       for (int i = 0; i < str1.length(); i++) {
+    	 count = 0;
          for (int j = i + 1; j < str1.length(); j++) {
-            if (carray[i] == carray[j]) {
-               System.out.print(carray[j] + " ");
-               break;
-            }
-         }
+        	 if(string[i] == string[j] && string[i] != ' ') {  
+                 count++;
+                 string[j] = '0';
+             }  
+         }    
+         if(count > 0 && string[i] != '0')  
+             System.out.println(string[i]); 
       }
 	 }
 }
